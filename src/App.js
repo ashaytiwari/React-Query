cc676d2b5a4d164598eb99a23f9b9ad45f5db5f9
ashaytiwari <ashaytiwari2001@gmail.com>
2021-12-31 11:41:54 +0530

    feat: Working on class base components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import React, { useState } from "react";
-import Planets from "./components/Planets";
-import Peoples from "./components/People";
-import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient();
+import Users from "./components/Users/Users";
 
 function App() {
-  const [page, setPage] = useState("planets");
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <h1>Star Wars Info</h1>
-        <Navbar setPage={setPage} />
-        <div className="content">
-          {page === "planets" ? <Planets /> : <Peoples />}
-        </div>
-      </div>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
+    <div>
+      <Users />
+    </div>
   );
 }
 
