b8709105122b02981adb11f127e1bd69c601a997
ashaytiwari <ashaytiwari2001@gmail.com>
2022-01-03 13:49:14 +0530

    feat: Custom Counter Hook implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import React, { useState } from "react";
-import Planets from "./components/Planets";
-import Peoples from "./components/People";
-import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient();
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
-  const [page, setPage] = useState("planets");
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <h1>Star Wars Info</h1>
-        <Navbar setPage={setPage} />
-        <div className="content">
-          {page === "planets" ? <Planets /> : <Peoples />}
-        </div>
-      </div>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
+    <React.Fragment>
+      <ForwardCounter />
+      <BackwardCounter />
+    </React.Fragment>
   );
 }
 
